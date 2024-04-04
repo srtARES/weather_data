507e270f4a992104da56350269de1529227a9cca
srtARES <163037741+srtARES@users.noreply.github.com>
2024-04-04 14:24:19 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import psycopg2
 import psycopg2.extras
-import os  # Import the os module
 
 app = Flask(__name__)
 CORS(app)
 
-
 def fetch_weather_data():
     try:
-        conn = psycopg2.connect("dbname=Assignment user=postgres password=SrtAres1904")
+        conn = psycopg2.connect("dbname=assignment_o9dk host=dpg-co5pcpm3e1ms73b9logg-a.oregon-postgres.render.com user=root password=UwrZkz3gvU6cayjibCpazPpkdcA4p1VO")
         cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cursor.execute("SELECT * FROM open_weather ORDER BY ID DESC")
         data = cursor.fetchall()
